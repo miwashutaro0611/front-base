225e908cbee970dcbbf19a65d43fbd2a03e873b5
miwashuntaro0611 <miwashuntaro19245@gmail.com>
2019-09-03 18:37:02 +0900

    初期のときにwatchしかなかったのを修正

--- gulpfile.js
+++ gulpfile.js
@@ -137,6 +137,6 @@ const watchFiles = () => {
   watch(srcPath.fonts, fontsFunc)
 }
 
-exports.default = parallel(watchFiles, browserSyncFunc)
+exports.default = parallel(watchFiles, [htmlFunc, stylusFunc, jsFunc, imageFunc, staticFunc, fontsFunc], browserSyncFunc)
 
 exports.build = parallel(htmlFunc, stylusFunc, jsFunc, imageFunc, staticFunc, fontsFunc)
