98839d4fa61d31091c916f1d0f0027f8b3697018
miwashutaro0611 <miwashuntaro19245@gmail>
2019-09-16 03:44:26 +0900

    マスク風アニメーションに変更

--- src/assets/js/Common/barba.ts
+++ src/assets/js/Common/barba.ts
@@ -1,5 +1,18 @@
 import barba from '@barba/core'
-import barbaCss from '@barba/css'
 
-barba.use(barbaCss)
-barba.init()
+const mask = document.querySelector('.mask')
+barba.init({
+  transitions: [
+    {
+      async leave() {
+        mask.classList.add('is-close')
+        await new Promise(resolve => {
+          return setTimeout(resolve, 1000)
+        })
+      },
+      afterEnter() {
+        mask.classList.remove('is-close')
+      },
+    },
+  ],
+})
