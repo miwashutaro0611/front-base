74af3bba80121f6a2b033510d5f40e3524dd37bd
miwashutaro0611 <miwashuntaro19245@gmail>
2019-09-17 00:52:46 +0900

    barbaをv1に変更

--- src/assets/js/Common/barba.ts
+++ src/assets/js/Common/barba.ts
@@ -1,18 +1,3 @@
-import barba from '@barba/core'
+import Barba from 'barba.js'
 
-const mask = document.querySelector('.mask')
-barba.init({
-  transitions: [
-    {
-      async leave() {
-        mask.classList.add('is-close')
-        await new Promise(resolve => {
-          return setTimeout(resolve, 1000)
-        })
-      },
-      afterEnter() {
-        mask.classList.remove('is-close')
-      },
-    },
-  ],
-})
+Barba.Pjax.start()
