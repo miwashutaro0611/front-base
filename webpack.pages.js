da19b7383b2ff3f450c6b11872cb806605bebb08
miwashutaro0611 <miwashuntaro19245@gmail.com>
2020-02-28 03:26:18 +0900

    abtutファイル追加

--- webpack.pages.js
+++ webpack.pages.js
@@ -3,7 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
+      filename: 'index.html',
       template: 'pug/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'about/index.html',
+      template: 'pug/about/index.pug'
     })
   ]
 }
