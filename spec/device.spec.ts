7c225a88bd1117d329ee5b427271bfe20336757f
miwashutaro0611 <miwashuntaro19245@gmail.com>
2020-06-27 00:17:52 +0900

    デバイスチェックテスト追加

---
+++ spec/device.spec.ts
@@ -0,0 +1,19 @@
+import UAParser from 'ua-parser-js'
+import device from '~/js/utils/device'
+
+const parser = new UAParser()
+const uastring =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'
+parser.setUA(uastring)
+
+test('DeviceCheck - Browser', () => {
+  expect(device(parser.getBrowser(), 'name')).toBe('Chrome')
+})
+
+test('DeviceCheck - Device', () => {
+  expect(device(parser.getDevice(), 'type')).toBe(undefined)
+})
+
+test('DeviceCheck - OS', () => {
+  expect(device(parser.getOS(), 'name')).toBe('Mac OS')
+})
--- src/assets/js/utils/device.ts
+++ src/assets/js/utils/device.ts
@@ -7,16 +7,20 @@
 import UAParser from 'ua-parser-js'
 
 const parser = new UAParser()
-const uaBrowser = parser.getBrowser() // ブラウザ判定
-const uaDevice = parser.getDevice() // デバイス判定
-const uaOs = parser.getOS() // os判定
+
+const check = (parserParam, obj) => {
+  console.dir(parserParam[obj])
+  return parserParam[obj]
+}
 
 window.addEventListener(
   'load',
   () => {
-    console.dir(uaBrowser)
-    console.dir(uaDevice)
-    console.dir(uaOs)
+    check(parser.getBrowser(), 'name') // ブラウザ判定 (name)
+    check(parser.getDevice(), 'type') // デバイス判定 (type)
+    check(parser.getOS(), 'name') // os判定 (name)
   },
   false
 )
+
+export default check
