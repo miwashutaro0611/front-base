e86774b04a12ca85bcb0daabd7019aca745eb3da
miwashutaro0611 <miwashuntaro19245@gmail.com>
2020-06-29 10:16:07 +0900

    headの内容書き換え

--- src/assets/js/modules/barba.ts
+++ src/assets/js/modules/barba.ts
@@ -9,6 +9,39 @@ barba.hooks.enter(() => {
   window.scrollTo(0, 0)
 })
 
+const replaceHeadTags = (target) => {
+  const head = document.head
+  const targetHead = target.html.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0]
+  const newPageHead = document.createElement('head')
+  newPageHead.innerHTML = targetHead
+  const removeHeadTags = [
+    "meta[name='keywords']",
+    "meta[name='description']",
+    "meta[property^='fb']",
+    "meta[property^='og']",
+    "meta[name^='twitter']",
+    "meta[name='robots']",
+    'meta[itemprop]',
+    'link[itemprop]',
+    "link[rel='prev']",
+    "link[rel='next']",
+    "link[rel='canonical']",
+  ].join(',')
+  const headTags = head.querySelectorAll(removeHeadTags)
+  headTags.forEach((item) => {
+    head.removeChild(item)
+  })
+  const newHeadTags = newPageHead.querySelectorAll(removeHeadTags)
+  newHeadTags.forEach((item) => {
+    head.appendChild(item)
+  })
+}
+
+barba.hooks.afterEnter(({ next }) => {
+  console.log(next)
+  replaceHeadTags(next)
+})
+
 barba.init()
 
 const eventDelete = (e) => {
