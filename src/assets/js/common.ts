deb4d6985179d401ec480137daf7a14ee775353b
miwashutaro0611 <miwashuntaro19245@gmail.com>
2021-02-02 00:14:43 +0900

    view-extraアップデート&細かな調整

--- src/assets/js/common.ts
+++ src/assets/js/common.ts
@@ -1,7 +1,8 @@
-import '~/stylus/style'
 import '~/js/utils/device'
 import '~/js/modules/picturefill'
 import '~/js/modules/alert'
 import '~/js/modules/what-input'
 import '~/js/modules/barba'
 import '~/js/modules/luxy'
+
+import '~/stylus/style'
--- src/assets/js/head.ts
+++ src/assets/js/head.ts
@@ -1,6 +1,2 @@
-import ViewportExtra from 'viewport-extra'
-
-// 最小幅だけ設定したい場合
-new ViewportExtra(375)
-// 最大幅を設定したい場合
-// new ViewportExtra({ minWidth: 360, maxWidth: 414 })
+import { setContent } from 'viewport-extra'
+setContent({ minWidth: 375 })
